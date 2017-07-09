52dbe3b8f1e79d749e864d1cbef1a7da17f1c19a
liwenqiu <liwenqiu@gmail.com>
2017-07-09 14:35:48 +0800

    Initial Commit

---
+++ MDictionary/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MDictionary
+//
+//  Created by liwenqiu on 09/07/2017.
+//  Copyright © 2017 liwenqiu. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
